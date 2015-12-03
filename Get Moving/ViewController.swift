5f2aa63d957aa92cdd100db6aa2f7e43f9490ab7
Ryan Cortez <ryancortez@me.com>
2015-12-02 22:51:42 -0600

    Initial Commit

---
+++ Get Moving/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Get Moving
+//
+//  Created by Ryan on 12/2/15.
+//  Copyright © 2015 Full Screen Ahead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
