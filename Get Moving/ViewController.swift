4a4487858f2ca5ad36960de4827acff0c9648233
Ryan Cortez <ryancortez@me.com>
2015-12-09 16:44:41 -0600

    Added constant update of steps from CoreMotion

--- Get Moving/ViewController.swift
+++ Get Moving/ViewController.swift
@@ -7,20 +7,70 @@
 //
 
 import UIKit
+import CoreMotion
+
+let pedometer = CMPedometer()
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var todaysSteps: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let pedometer = Pedometer()
-        pedometer.getPedometerData()
+        getPedometerData()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    var stepsTaken = NSNumber(int: 0)
+    
+    func getPedometerData (){
+        
+        
+        let currentDate = NSDate()
+        let startDate = NSDate(timeIntervalSinceNow: -60*60*12)
+        
+        
+        
+        pedometer.queryPedometerDataFromDate(startDate, toDate: currentDate, withHandler: {
+            data, error in
+            
+            if (error == nil) {
+                if (data != nil) {
+                    self.stepsTaken = (data?.numberOfSteps)!
+                    
+                    print("stepsTaken: \(self.stepsTaken)")
+                    print("startDate: \(data?.startDate)")
+                    print("endDate: \(data?.endDate)")
+                    print("distance: \(data?.distance)")
+                    print("currentCadence: \(data?.currentCadence)")
+                    print("currentPace: \(data?.currentPace)")
+                    print("floorsAscended: \(data?.floorsAscended)")
+                    print("floorsDescended: \(data?.floorsDescended)")
+                }
+            }
+            else{
+                print("There was an error retrieving the pedometer data: \(error.debugDescription)")
+            }
+        })
+        
+        pedometer.startPedometerUpdatesFromDate(currentDate, withHandler: {data, error in
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if(error == nil){
+                    self.todaysSteps.text = "Today: \(data?.numberOfSteps)"
+                }
+            })
+            
+        })
+        
+    }
+    
 }
 
