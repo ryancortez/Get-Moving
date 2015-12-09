e01ac9b15723c349af2361932e5af1364b319ed8
Ryan Cortez <ryancortez@me.com>
2015-12-09 16:24:40 -0600

    Confirmed that I can pull data from CoreMotion

--- Get Moving/ViewController.swift
+++ Get Moving/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let pedometer = Pedometer()
+        pedometer.getPedometerData()
     }
 
     override func didReceiveMemoryWarning() {
