c302062aa1db3aea44f13fe6d63c8460f925f379
Ryan Cortez <ryancortez@me.com>
2015-12-12 23:08:06 -0600

    Added the drawing of line the for the line chart with a maintainable alogrithim

--- Get Moving/ChartView.swift
+++ Get Moving/ChartView.swift
@@ -14,32 +14,60 @@ import UIKit
 class ChartView: UIView {
     
     override func drawRect(rect: CGRect) {
-        //set up the width and height variables
-        //for the horizontal stroke
-        let plusHeight: CGFloat = 2.0
-        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 2.0
         
-        //create the path
-        let plusPath = UIBezierPath()
+        // Number of columns wanted in the line chart
+        let numberOfColumns = 7
+        let lineChartLineHeight: CGFloat = 2.0
+        let lineChartLineWidth: CGFloat = min(bounds.width, bounds.height) * 2.0
         
-        //set the path's line width to the height of the stroke
-        plusPath.lineWidth = plusHeight
+        // Divide the chart into the number of columns
+        let sidesOfColumnsBuffer = 20
+        let maximumLineWidth = bounds.width - (CGFloat(sidesOfColumnsBuffer) * 2)
+        let columnWidth = maximumLineWidth / CGFloat(numberOfColumns)
         
-        //move the initial point of the path
-        //to the start of the horizontal stroke
-        plusPath.moveToPoint(CGPoint(
-            x:bounds.width/2 - plusWidth/2,
-            y:bounds.height/2))
+        // Create a path
+        let lineChartPath = UIBezierPath()
         
-        //add a point to the path at the end of the stroke
-        plusPath.addLineToPoint(CGPoint(
-            x:bounds.width/2 + plusWidth/2,
-            y:bounds.height/2))
+        lineChartPath.lineWidth = lineChartLineHeight
         
-        //set the stroke color
+        
+        // Place the start of the path at the left of the line chart
+        lineChartPath.moveToPoint(CGPoint(x: CGFloat(sidesOfColumnsBuffer), y: bounds.height / 2))
+        
+        lineChartPath.addLineToPoint(CGPoint(x: bounds.width - CGFloat(sidesOfColumnsBuffer), y: bounds.height / 3))
+        
+        // Setting line chart's color and drawing the line
         UIColor.blueColor().setStroke()
+        lineChartPath.stroke()
         
-        //draw the stroke
-        plusPath.stroke()
+// Hiding this example line drawing for now
+
+//        //set up the width and height variables
+//        //for the horizontal stroke
+//        let plusHeight: CGFloat = 2.0
+//        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 2.0
+//        
+//        //create the path
+//        let plusPath = UIBezierPath()
+//        
+//        //set the path's line width to the height of the stroke
+//        plusPath.lineWidth = plusHeight
+//        
+//        //move the initial point of the path
+//        //to the start of the horizontal stroke
+//        plusPath.moveToPoint(CGPoint(
+//            x:bounds.width/2 - plusWidth/2,
+//            y:bounds.height/2))
+//        
+//        //add a point to the path at the end of the stroke
+//        plusPath.addLineToPoint(CGPoint(
+//            x:bounds.width/2 + plusWidth/2,
+//            y:bounds.height/2))
+//        
+//        //set the stroke color
+//        UIColor.blueColor().setStroke()
+//        
+//        //draw the stroke
+//        plusPath.stroke()
     }
 }
